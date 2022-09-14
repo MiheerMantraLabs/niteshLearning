5992a055b671fc0dfaa992583c553a35ce60285b
MiheerMantraLabs <110654852+MiheerMantraLabs@users.noreply.github.com>
2022-09-15 01:20:07 +0530

    Lacture 2 Data Types

---
+++ 2_lacture_data_type.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void main(){
+	int a=5; //Number or intereger data type
+	float b = 0.5; //float data type
+	char c[] = "Hello world"; //string or char data type
+	
+//	Print integer or number datatype values
+printf("--------------------------\n");
+printf("Integere type data : %d\n",a);
+
+//	Print Float datatype values
+printf("--------------------------\n");
+printf("Float type data : %0.2f\n",b);
+
+//	Print String or charecter datatype values
+printf("--------------------------\n");
+printf("String type data : %s",c);
+
+}
